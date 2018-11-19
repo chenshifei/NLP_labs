b3e81a47545519a3c3dec257a1e9392c13f5c35f
shifei.chen <chenshifei.j@gmail.com>
2018-11-19 11:05:40 +0100

    Optimize tokenizer 1

--- NLP_Lab_2/tokenizer1-sent.py
+++ NLP_Lab_2/tokenizer1-sent.py
@@ -2,17 +2,20 @@ import re, sys
 
 for line in sys.stdin:
     regex = re.compile(r'''
-        ((?<=\.)\s(?=[A-Z])|
-        \d+\.\d+|
-        n\'t|
-        \'[sd]|
-        (\w+-)+\w+|
-        [A-Z]\w*\.(\w+\.)*|
-        \'\'?|
-        [,;:.!?\"%$&]|
-        --|
-        \w+(?=n\'t)|
-        \w+)
+        (
+            (?<=\.|\"|`|\?|\!)\s+(?=[A-Z]|\"|`|$)|
+            \d+\.\d+|
+            n\'t|
+            \'[sd]|
+            (\w+-)+\w+|
+            ([A-Z]\w{0,3}\.)+|
+            [,;:.!?\"%$&]|
+            --|
+            ``|
+            \'\'?|
+            \w+(?=n\'t)|
+            \w+
+        )
     ''', re.VERBOSE)
     for token in re.findall(regex, line):
         if token[0].isspace():
